a4c5d03f266906483616920697f110237121b2f0
Miguel-bc <miguel.blaya@gmail.com>
2026-03-21 18:49:39 +0100

    Carga Inicial

---
+++ Proyecto/mi_app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+
+def inicio(request):
+    return HttpResponse("Hola Miguel, Django está funcionando 🚀")
+
